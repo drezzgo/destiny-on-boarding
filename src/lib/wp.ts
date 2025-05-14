2c5e17f7449686f5380023b371f528d24636c66a
Andres Gongora <agongoraramirez@gmail.com>
2025-05-14 14:47:46 -0500

    feat: Integrate Testimonials component with dynamic props and update getPageInfo to include testimonial data

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -14,9 +14,20 @@ export const getPageInfo = async (slug: string) => {
         acf: {subtitle_how_it_works},
         acf: {text_how_it_works},
         acf: {title_card_1_how_it_works},
-        acf: {paragraph_card_1_how_it_works}
+        acf: {paragraph_card_1_how_it_works},
+        acf: {title_wpas},
+        acf: {paragraph_wpas},
+        acf: {paragraph_testimonial_1_wpas},
+        acf: {paragraph_testimonial_2_wpas},
+        acf: {paragraph_testimonial_3_wpas},
+        acf: {name_1_wpas},
+        acf: {name_2_wpas},
+        acf: {name_3_wpas},
+        acf: {job_1_wpas},
+        acf: {job_2_wpas},
+        acf: {job_3_wpas}
     } = data;
     console.log(data);
 
-    return { title, hero_paragraph, button, text1_how_it_works, subtitle_how_it_works, text_how_it_works, title_card_1_how_it_works, paragraph_card_1_how_it_works };
+    return { title, hero_paragraph, button, text1_how_it_works, subtitle_how_it_works, text_how_it_works, title_card_1_how_it_works, paragraph_card_1_how_it_works, title_wpas, paragraph_wpas, paragraph_testimonial_1_wpas, paragraph_testimonial_2_wpas, paragraph_testimonial_3_wpas, name_1_wpas, name_2_wpas, name_3_wpas, job_1_wpas, job_2_wpas, job_3_wpas };
 }
