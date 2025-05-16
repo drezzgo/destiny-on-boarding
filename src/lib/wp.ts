22f3be486a9ea937f4f91db182b5f17fe9795a59
Andres Gongora <agongoraramirez@gmail.com>
2025-05-15 22:28:43 -0500

    feat: Update PopularPrograms and Testimonials components to enhance data handling and layout; integrate new props for dynamic content

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -26,8 +26,22 @@ export const getPageInfo = async (slug: string) => {
         acf: {paragraph_testimonial_3},
         acf: {name_testimonial_3},
         acf: {job_testimonial_3},
+        acf: {title_pp},
+        acf: {paragraph_pp},
+        acf: {name_program_1},
+        acf: {paragraph_program_1},
+        acf: {extend_paragraph_program_1},
+        acf: {name_program_2},
+        acf: {paragraph_program_2},
+        acf: {extend_paragraph_program_2},
+        acf: {name_program_3},
+        acf: {paragraph_program_3},
+        acf: {extend_paragraph_program_3},
+        acf: {name_program_4},
+        acf: {paragraph_program_4},
+        acf: {extend_paragraph_program_4},
     } = data;
     console.log(data);
 
-    return { title, hero_paragraph, button, text1_how_it_works, subtitle_how_it_works, text_how_it_works, title_card_1_how_it_works, paragraph_card_1_how_it_works, title_wpas, paragraph_wpas, paragraph_testimonial_1, name_testimonial_1, job_testimonial_1, paragraph_testimonial_2, name_testimonial_2, job_testimonial_2, paragraph_testimonial_3, name_testimonial_3, job_testimonial_3 };
+    return { title, hero_paragraph, button, text1_how_it_works, subtitle_how_it_works, text_how_it_works, title_card_1_how_it_works, paragraph_card_1_how_it_works, title_wpas, paragraph_wpas, paragraph_testimonial_1, name_testimonial_1, job_testimonial_1, paragraph_testimonial_2, name_testimonial_2, job_testimonial_2, paragraph_testimonial_3, name_testimonial_3, job_testimonial_3, title_pp, paragraph_pp, name_program_1, paragraph_program_1, extend_paragraph_program_1, name_program_2, paragraph_program_2, extend_paragraph_program_2, name_program_3, paragraph_program_3, extend_paragraph_program_3, name_program_4, paragraph_program_4, extend_paragraph_program_4 };
 }
