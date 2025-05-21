ae2ef7dd63075593a9a3b64a17959b6f4f05ab41
Andres Gongora <agongoraramirez@gmail.com>
2025-05-20 23:06:13 -0500

    fix: Update header link to point to the correct URL and enhance fetch request with cache control

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -3,7 +3,10 @@ const apiUrl = `https://destinyonboard.com/wp-json/wp/v2`; // Cambiado a backtic
 const apiUrlAcf = `${domain}/wp-json/acf/v3`;
 
 export const getPageInfo = async (slug: string) => {
-    const response = await fetch(`${apiUrl}/pages?slug=${slug}`);
+    const response = await fetch(`${apiUrl}/pages?slug=${slug}`, {
+        cache: "no-store" // fuerza una petición nueva
+    });
+
     if (!response.ok) throw new Error('Failed to fetch page info');
     const [data] = await response.json();
     const { 
